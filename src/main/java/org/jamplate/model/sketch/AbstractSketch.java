09519769324a8dd61a3ba32d78cd72b23523b411
Sulaiman Oboody <lsafer@cufy.org>
2021-01-12 22:55:06 +0300

    Removed commented code

--- src/main/java/org/jamplate/model/sketch/AbstractSketch.java
+++ src/main/java/org/jamplate/model/sketch/AbstractSketch.java
@@ -33,127 +33,17 @@ import java.util.TreeSet;
  * @since 0.0.2 ~2021.01.07
  */
 public abstract class AbstractSketch implements Sketch {
-	protected final Source source;
-	//	/**
-	//	 * The sketches in this sketch.
-	//	 * <br>
-	//	 * It shall be always sorted.
-	//	 * <br>
-	//	 * It shall have no sketch that has a source that {@link Source#contains(Source)} or {@link Source#clashWith(Source)} with any source of another
-	//	 * contained sketch.
-	//	 *
-	//	 * @since 0.0.2 ~2021.01.7
-	//	 */
-	//	protected final SortedSet<Sketch> sketches = new TreeSet<>(Sketch.COMPARATOR);
-	//	/**
-	//	 * The source of this sketch.
-	//	 *
-	//	 * @since 0.0.2 ~2021.01.7
-	//	 */
-	//	protected final Source source;
-	//
-	//	/**
-	//	 * Construct a new abstract sketch.
-	//	 *
-	//	 * @param source the source of this abstract sketch.
-	//	 * @throws NullPointerException if the given {@code source} is null.
-	//	 * @since 0.0.2 ~2021.01.7
-	//	 */
-	//	protected AbstractSketch(Source source) {
-	//		Objects.requireNonNull(source, "source");
-	//		this.source = source;
-	//	}
-	//
-	//	@Override
-	//	public boolean check(Sketch sketch) {
-	//		Objects.requireNonNull(sketch, "sketch");
-	//		return this.source.contains(sketch.source()) &&
-	//			   this.sketches.stream()
-	//					   .noneMatch(s ->
-	//							   s.source().clashWith(sketch.source())
-	//					   );
-	//	}
-	//
-	//	@Override
-	//	public void put(Sketch sketch) {
-	//		Objects.requireNonNull(sketch, "sketch");
-	//		if (this.check(sketch))
-	//			throw new IllegalArgumentException("Sketch rejected");
-	//
-	//		this.sketches.add(sketch);
-	//		Source source = sketch.source();
-	//		//		this.sketches.removeIf(sk -> {
-	//		//
-	//		//		});
-	//	}
-	//
-	//	@Override
-	//	public Source source() {
-	//		return this.source;
-	//	}
-	//
-	//	@Override
-	//	public void visit(Visitor<Sketch> visitor) {
-	//		//		this.reserved.entrySet()
-	//		//				.stream()
-	//		//				.sorted(Comparator.comparingLong(e -> e.getKey()[0]))
-	//		//				.map(Map.Entry::getValue)
-	//		//				.distinct()
-	//		//				.forEach(/visitor::visit);
-	//	}
-
 	/**
 	 * <br>
 	 * The elements of this set must all have a dominance of {@link Dominance#PART} with
 	 * this sketch.
 	 */
-	protected SortedSet<Sketch> sketches = new TreeSet<>(Sketch.COMPARATOR);
+	protected final Source source;
 
 	public AbstractSketch(Source source) {
 		Objects.requireNonNull(source, "source");
 		this.source = source;
 	}
-
-	//	@Override
-	//	public boolean available() {
-	//		if (this.sketches.stream().anyMatch(Sketch::available))
-	//			return true;
-	//
-	//		//assert that `this.sketches` is sorted and has no clashes
-	//		Iterator<Sketch> iterator = this.sketches.iterator();
-	//
-	//		if (iterator.hasNext()) {
-	//			Sketch sketch = iterator.next();
-	//
-	//			if (iterator.hasNext()) {
-	//				if (Sketch.relation(this, sketch) == Relation.START) {
-	//					do {
-	//						Sketch s = iterator.next();
-	//
-	//						if (Sketch.relation(sketch, s) != Relation.NEXT)
-	//							//a one didn't reserved from its previous -> there is a gap
-	//							return true;
-	//					} while (iterator.hasNext());
-	//
-	//					//the last one isn't at end -> there is a gap
-	//					return Sketch.relation(this, sketch) != Relation.END;
-	//				}
-	//
-	//				//first didn't reserved from the start -> there is a gap
-	//				return true;
-	//			}
-	//
-	//			//just one reserved -> that one is not reserving everything
-	//			return Sketch.dominance(this, sketch) != Dominance.EXACT;
-	//		}
-	//
-	//		//nothing reserved -> this is not empty
-	//		return this.source.length() != 0;
-	//	}
-	//
-	//	public Sketch available() {
-	//
-	//	}
 
 	@SuppressWarnings("OverlyComplexMethod")
 	@Override
