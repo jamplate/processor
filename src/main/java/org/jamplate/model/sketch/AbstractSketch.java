c35217c476d7131cd114fff44be4980b38b38c6a
Sulaiman Oboody <lsafer@cufy.org>
2021-01-12 22:22:31 +0300

    Added basic functionalities to the class `Sketch`
    - Added `methods dominance`, `methods relation` for easier access
    - Added `accept` for visitor style parsing
    - Added `put` for validate-autowire-add

--- src/main/java/org/jamplate/model/sketch/AbstractSketch.java
+++ src/main/java/org/jamplate/model/sketch/AbstractSketch.java
@@ -15,79 +15,243 @@
  */
 package org.jamplate.model.sketch;
 
+import org.jamplate.model.source.Dominance;
+import org.jamplate.model.source.Relation;
+import org.jamplate.model.source.Source;
+
+import java.util.Iterator;
+import java.util.Objects;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 /**
- * An abstract of the interface {@link Sketch} that implements the basic functionality of a sketch.
+ * An abstract of the interface {@link Sketch} that implements the basic functionality of
+ * a sketch.
  *
  * @author LSafer
  * @version 0.0.2
  * @since 0.0.2 ~2021.01.07
  */
 public abstract class AbstractSketch implements Sketch {
-//	/**
-//	 * The sketches in this sketch.
-//	 * <br>
-//	 * It shall be always sorted.
-//	 * <br>
-//	 * It shall have no sketch that has a source that {@link Source#contains(Source)} or {@link Source#clashWith(Source)} with any source of another
-//	 * contained sketch.
-//	 *
-//	 * @since 0.0.2 ~2021.01.7
-//	 */
-//	protected final SortedSet<Sketch> sketches = new TreeSet<>(Sketch.COMPARATOR);
-//	/**
-//	 * The source of this sketch.
-//	 *
-//	 * @since 0.0.2 ~2021.01.7
-//	 */
-//	protected final Source source;
-//
-//	/**
-//	 * Construct a new abstract sketch.
-//	 *
-//	 * @param source the source of this abstract sketch.
-//	 * @throws NullPointerException if the given {@code source} is null.
-//	 * @since 0.0.2 ~2021.01.7
-//	 */
-//	protected AbstractSketch(Source source) {
-//		Objects.requireNonNull(source, "source");
-//		this.source = source;
-//	}
-//
-//	@Override
-//	public boolean check(Sketch sketch) {
-//		Objects.requireNonNull(sketch, "sketch");
-//		return this.source.contains(sketch.source()) &&
-//			   this.sketches.stream()
-//					   .noneMatch(s ->
-//							   s.source().clashWith(sketch.source())
-//					   );
-//	}
-//
-//	@Override
-//	public void put(Sketch sketch) {
-//		Objects.requireNonNull(sketch, "sketch");
-//		if (this.check(sketch))
-//			throw new IllegalArgumentException("Sketch rejected");
-//
-//		this.sketches.add(sketch);
-//		Source source = sketch.source();
-//		//		this.sketches.removeIf(sk -> {
-//		//
-//		//		});
-//	}
-//
-//	@Override
-//	public Source source() {
-//		return this.source;
-//	}
-//
-//	@Override
-//	public void visit(Visitor<Sketch> visitor) {
-//		//		this.reserved.entrySet()
-//		//				.stream()
-//		//				.sorted(Comparator.comparingLong(e -> e.getKey()[0]))
-//		//				.map(Map.Entry::getValue)
-//		//				.distinct()
-//		//				.forEach(/visitor::visit);
-//	}
+	protected final Source source;
+	//	/**
+	//	 * The sketches in this sketch.
+	//	 * <br>
+	//	 * It shall be always sorted.
+	//	 * <br>
+	//	 * It shall have no sketch that has a source that {@link Source#contains(Source)} or {@link Source#clashWith(Source)} with any source of another
+	//	 * contained sketch.
+	//	 *
+	//	 * @since 0.0.2 ~2021.01.7
+	//	 */
+	//	protected final SortedSet<Sketch> sketches = new TreeSet<>(Sketch.COMPARATOR);
+	//	/**
+	//	 * The source of this sketch.
+	//	 *
+	//	 * @since 0.0.2 ~2021.01.7
+	//	 */
+	//	protected final Source source;
+	//
+	//	/**
+	//	 * Construct a new abstract sketch.
+	//	 *
+	//	 * @param source the source of this abstract sketch.
+	//	 * @throws NullPointerException if the given {@code source} is null.
+	//	 * @since 0.0.2 ~2021.01.7
+	//	 */
+	//	protected AbstractSketch(Source source) {
+	//		Objects.requireNonNull(source, "source");
+	//		this.source = source;
+	//	}
+	//
+	//	@Override
+	//	public boolean check(Sketch sketch) {
+	//		Objects.requireNonNull(sketch, "sketch");
+	//		return this.source.contains(sketch.source()) &&
+	//			   this.sketches.stream()
+	//					   .noneMatch(s ->
+	//							   s.source().clashWith(sketch.source())
+	//					   );
+	//	}
+	//
+	//	@Override
+	//	public void put(Sketch sketch) {
+	//		Objects.requireNonNull(sketch, "sketch");
+	//		if (this.check(sketch))
+	//			throw new IllegalArgumentException("Sketch rejected");
+	//
+	//		this.sketches.add(sketch);
+	//		Source source = sketch.source();
+	//		//		this.sketches.removeIf(sk -> {
+	//		//
+	//		//		});
+	//	}
+	//
+	//	@Override
+	//	public Source source() {
+	//		return this.source;
+	//	}
+	//
+	//	@Override
+	//	public void visit(Visitor<Sketch> visitor) {
+	//		//		this.reserved.entrySet()
+	//		//				.stream()
+	//		//				.sorted(Comparator.comparingLong(e -> e.getKey()[0]))
+	//		//				.map(Map.Entry::getValue)
+	//		//				.distinct()
+	//		//				.forEach(/visitor::visit);
+	//	}
+
+	/**
+	 * <br>
+	 * The elements of this set must all have a dominance of {@link Dominance#PART} with
+	 * this sketch.
+	 */
+	protected SortedSet<Sketch> sketches = new TreeSet<>(Sketch.COMPARATOR);
+
+	public AbstractSketch(Source source) {
+		Objects.requireNonNull(source, "source");
+		this.source = source;
+	}
+
+	//	@Override
+	//	public boolean available() {
+	//		if (this.sketches.stream().anyMatch(Sketch::available))
+	//			return true;
+	//
+	//		//assert that `this.sketches` is sorted and has no clashes
+	//		Iterator<Sketch> iterator = this.sketches.iterator();
+	//
+	//		if (iterator.hasNext()) {
+	//			Sketch sketch = iterator.next();
+	//
+	//			if (iterator.hasNext()) {
+	//				if (Sketch.relation(this, sketch) == Relation.START) {
+	//					do {
+	//						Sketch s = iterator.next();
+	//
+	//						if (Sketch.relation(sketch, s) != Relation.NEXT)
+	//							//a one didn't reserved from its previous -> there is a gap
+	//							return true;
+	//					} while (iterator.hasNext());
+	//
+	//					//the last one isn't at end -> there is a gap
+	//					return Sketch.relation(this, sketch) != Relation.END;
+	//				}
+	//
+	//				//first didn't reserved from the start -> there is a gap
+	//				return true;
+	//			}
+	//
+	//			//just one reserved -> that one is not reserving everything
+	//			return Sketch.dominance(this, sketch) != Dominance.EXACT;
+	//		}
+	//
+	//		//nothing reserved -> this is not empty
+	//		return this.source.length() != 0;
+	//	}
+	//
+	//	public Sketch available() {
+	//
+	//	}
+
+	@SuppressWarnings("OverlyComplexMethod")
+	@Override
+	public boolean accept(SketchVisitor visitor) {
+		Objects.requireNonNull(visitor, "visitor");
+
+		if (visitor.visitSketch(this))
+			return true;
+
+		Iterator<Sketch> iterator = this.sketches.iterator();
+
+		if (iterator.hasNext()) {
+			Sketch last = iterator.next();
+
+			if (Sketch.relation(this, last) != Relation.START)
+				//There is a gap between the start and the first sketch
+				if (visitor.visitSource(this, Source.cutStartStart(
+						this.source,
+						last.source()
+				)))
+					return true;
+
+			if (last.accept(visitor))
+				return true;
+
+			while (iterator.hasNext()) {
+				Sketch next = iterator.next();
+
+				if (Sketch.relation(last, next) != Relation.NEXT)
+					//There is a gap between the two sketches
+					if (visitor.visitSource(this, Source.cutEndStart(
+							last.source(),
+							next.source()
+					)))
+						return true;
+
+				if (next.accept(visitor))
+					return true;
+
+				last = next;
+			}
+
+			if (Sketch.relation(this, last) != Relation.END)
+				//There is a gap between the last sketch and the end
+				return visitor.visitSource(this, Source.cutEndEnd(
+						last.source(),
+						this.source
+				));
+
+			return false;
+		}
+
+		//The whole source is available
+		return visitor.visitSource(this, this.source);
+	}
+
+	@Override
+	public void put(Sketch sketch) {
+		Objects.requireNonNull(sketch, "sketch");
+		if (Sketch.dominance(this, sketch) != Dominance.PART)
+			throw new IllegalArgumentException("Cannot put a sketch with a dominance other than PART");
+
+		//clash check...
+		for (Sketch next : this.sketches) {
+			Dominance dominance = Sketch.dominance(next, sketch);
+
+			if (dominance == Dominance.SHARE || dominance == Dominance.EXACT)
+				throw new IllegalStateException("Sketch Clash");
+		}
+
+		//part/contain check...
+		Iterator<Sketch> iterator = this.sketches.iterator();
+		while (iterator.hasNext()) {
+			Sketch next = iterator.next();
+
+			Dominance dominance = Sketch.dominance(sketch, next);
+
+			if (dominance == Dominance.CONTAIN) {
+				next.put(sketch);
+				return;
+			}
+			if (dominance == Dominance.PART) {
+				iterator.remove();
+				sketch.put(next);
+			}
+		}
+
+		//NONE
+		this.sketches.add(sketch);
+	}
+
+	@Override
+	public Source source() {
+		return this.source;
+	}
+
+	@Override
+	public String toString() {
+		return this.getClass().getSimpleName() + " (" + this.source + ")";
+	}
 }
--- src/main/java/org/jamplate/model/sketch/Sketch.java
+++ src/main/java/org/jamplate/model/sketch/Sketch.java
@@ -15,7 +15,8 @@
  */
 package org.jamplate.model.sketch;
 
-import org.jamplate.Node;
+import org.jamplate.model.source.Dominance;
+import org.jamplate.model.source.Relation;
 import org.jamplate.model.source.Source;
 
 import java.util.Comparator;
@@ -23,13 +24,14 @@ import java.util.Comparator;
 /**
  * A temporary sketch containing initial thoughts about an element.
  * <br>
- * Note: sketches are built from bottom to top. So, a typical sketch will store its sub-sketches but never its parent sketch.
+ * Note: sketches are built from bottom to top. So, a typical sketch will store its
+ * sub-sketches but never its parent sketch.
  *
  * @author LSafer
  * @version 0.0.2
  * @since 0.0.2 ~2020.12.25
  */
-public interface Sketch extends Node<Sketch> {
+public interface Sketch {
 	/**
 	 * The standard sketch comparator.
 	 *
@@ -38,12 +40,155 @@ public interface Sketch extends Node<Sketch> {
 	Comparator<Sketch> COMPARATOR = Comparator.comparing(Sketch::source, Source.COMPARATOR);
 
 	/**
-	 * Returns true, if the source of this sketch has an area that has not been reserved by any sub-sketches of this sketch.
+	 * Calculate how dominant the given area {@code [s, e)} over the given {@code
+	 * sketch}.
 	 *
-	 * @return true, if this sketch has an un-sketched area.
-	 * @since 0.0.2 ~2021.01.9
+	 * @param sketch the sketch. (the first area)
+	 * @param s      the first index of the second area.
+	 * @param e      one past the last index of the second area.
+	 * @return how dominant the second area over the given sketch.
+	 * @throws NullPointerException     if the given {@code sketch} is null.
+	 * @throws IllegalArgumentException if the given {@code s} is not in the range {@code
+	 *                                  [0, e]}.
+	 * @see Dominance#compute(int, int, int, int)
+	 * @since 0.0.2 ~2021.01.11
 	 */
-	boolean available();
+	static Dominance dominance(Sketch sketch, int s, int e) {
+		return Source.dominance(sketch.source(), s, e);
+	}
+
+	/**
+	 * Calculate how dominant the given {@code other} sketch over the given {@code
+	 * sketch}.
+	 *
+	 * @param sketch the sketch. (the first area)
+	 * @param other  the other sketch. (the second area)
+	 * @return how dominant the second sketch over the first sketch.
+	 * @throws NullPointerException if the given {@code sketch} or {@code other} is null.
+	 * @see Dominance#compute(int, int, int, int)
+	 * @since 0.0.2 ~2021.01.11
+	 */
+	static Dominance dominance(Sketch sketch, Sketch other) {
+		return Source.dominance(sketch.source(), other.source());
+	}
+
+	/**
+	 * Calculate the relation between the given {@code sketch} and the area {@code [s,
+	 * e)}.
+	 *
+	 * @param sketch the sketch. (the first area)
+	 * @param s      the first index of the second area.
+	 * @param e      one past the last index of the second area.
+	 * @return the relation between the given sketch and the given area.
+	 * @throws NullPointerException     if the given {@code sketch} is null.
+	 * @throws IllegalArgumentException if the given {@code s} is not in the range {@code
+	 *                                  [s, e]}.
+	 * @see Relation#compute(int, int, int, int)
+	 * @since 0.0.2 ~2021.01.11
+	 */
+	static Relation relation(Sketch sketch, int s, int e) {
+		return Source.relation(sketch.source(), s, e);
+	}
+
+	/**
+	 * Calculate the relation between the given {@code sketch} and the {@code other}
+	 * sketch.
+	 *
+	 * @param sketch the sketch. (the first area)
+	 * @param other  the other sketch. (the second area)
+	 * @return the relation between the given sketch and the given other sketch.
+	 * @throws NullPointerException if the given {@code sketch} or {@code other} is null.
+	 * @see Relation#compute(int, int, int, int)
+	 * @since 0.0.2 ~2021.01.11
+	 */
+	static Relation relation(Sketch sketch, Sketch other) {
+		return Source.relation(sketch.source(), other.source());
+	}
+
+	@Override
+	int hashCode();
+
+	/**
+	 * Returns a string representation of this sketch.
+	 * <pre>
+	 *     {@code <Name>} ({@link #source() &lt;source()&gt;})
+	 * </pre>
+	 *
+	 * @return a string representation of this sketch.
+	 * @since 0.0.2 ~2021.01.10
+	 */
+	@Override
+	String toString();
+
+	/**
+	 * Recursively loop down in the tree and invoke the appropriate method for each
+	 * element met. The invocation shall always be ordered from the first to the last and
+	 * from the containers to the parts.
+	 * <br>
+	 * Exceptions thrown by the given {@code visitor} will not be caught.
+	 *
+	 * @param visitor the visitor to be invoked for each element.
+	 * @return true, if the given {@code visitor} wishes to stop the loop.
+	 * @throws NullPointerException if the given {@code visitor} is null.
+	 * @since 0.0.2 ~2021.01.11
+	 */
+	boolean accept(SketchVisitor visitor);
+
+	/**
+	 * Put the given {@code sketch} to this sketch. Putting a sketch into another sketch
+	 * is like telling that other sketch to mark its place as reserved. If the given
+	 * sketch is a {@link Dominance#PART} with a sketch in this sketch. Then the given
+	 * {@code sketch} should be put into that sketch instead. On the other hand, if a
+	 * sketch in this sketch has a dominance of {@link Dominance#PART} with the given
+	 * {@code sketch}. Then this sketch will transfer than sketch to the given {@code
+	 * sketch}. (unless a clash happened, then an exception thrown and nothing happens)
+	 *
+	 * @param sketch the sketch to be put.
+	 * @throws NullPointerException     if the given {@code sketch} is null.
+	 * @throws IllegalStateException    if the given {@code sketch} is clashing with a
+	 *                                  previously reserved area (has a dominance of
+	 *                                  either {@link Dominance#SHARE} or {@link
+	 *                                  Dominance#EXACT}).
+	 * @throws IllegalArgumentException if the given {@code sketch} has a dominance other
+	 *                                  than {@link Dominance#PART} with this sketch.
+	 * @since 0.0.2 ~2021.01.12
+	 */
+	void put(Sketch sketch);
+
+	/**
+	 * Get the source of this sketch.
+	 *
+	 * @return the source of this sketch.
+	 * @since 0.0.2 ~2021.01.7
+	 */
+	Source source();
+}
+//	/**
+//	 * Transfer the given {@code child} sketch to the given {@code parent} sketch. The given {@code child} sketch MUST be a sketch that this sketch
+//	 * contain. The {@code parent} sketch must be able to {@link #put(Sketch)} the given {@code child} sketch in it.
+//	 *
+//	 * @param parent the sketch to be the new parent for the given {@code child} sketch.
+//	 * @param child
+//	 * @throws IllegalArgumentException if {@link Source#contains(Source) parent.source().contains(child.source())} returned false.
+//	 * @throws NullPointerException     if the given {@code parent} or {@code child} is null.
+//	 * @since 0.0.2 ~2021.01.9
+//	 */
+//	void transfer(Sketch parent, Sketch child);
+
+//	void remove(Sketch sketch);
+//
+//	boolean check(Source source);
+//
+//	boolean check(Sketch sketch);
+//
+//	/**
+//	 * Put the given sketch in this sketch.
+//	 *
+//	 * @param sketch the sketch to be put.
+//	 * @throws NullPointerException if the given {@code sketch} is null.
+//	 * @since 0.0.2 ~2021.01.7
+//	 */
+//	void put(Sketch sketch);
 //
 //	/**
 //	 * Check if the given {@code sketch} can be sub sketch of this.
@@ -60,35 +205,3 @@ public interface Sketch extends Node<Sketch> {
 //	 * @since 0.0.2 ~2021.01.9
 //	 */
 //	boolean check(Sketch sketch);
-
-	/**
-	 * Put the given sketch in this sketch.
-	 *
-	 * @param sketch the sketch to be put.
-	 * @throws NullPointerException     if the given {@code sketch} is null.
-	 * @since 0.0.2 ~2021.01.7
-	 */
-	void put(Sketch sketch);
-
-//	void remove(Sketch sketch);
-
-	/**
-	 * Get the source of this sketch.
-	 *
-	 * @return the source of this sketch.
-	 * @since 0.0.2 ~2021.01.7
-	 */
-	Source source();
-
-//	/**
-//	 * Transfer the given {@code child} sketch to the given {@code parent} sketch. The given {@code child} sketch MUST be a sketch that this sketch
-//	 * contain. The {@code parent} sketch must be able to {@link #put(Sketch)} the given {@code child} sketch in it.
-//	 *
-//	 * @param parent the sketch to be the new parent for the given {@code child} sketch.
-//	 * @param child
-//	 * @throws IllegalArgumentException if {@link Source#contains(Source) parent.source().contains(child.source())} returned false.
-//	 * @throws NullPointerException     if the given {@code parent} or {@code child} is null.
-//	 * @since 0.0.2 ~2021.01.9
-//	 */
-//	void transfer(Sketch parent, Sketch child);
-}
