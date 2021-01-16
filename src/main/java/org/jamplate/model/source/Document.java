5c97e6feaefa1d8a63d8a5001a16803a3ce1103a
Sulaiman Oboody <lsafer@cufy.org>
2021-01-16 21:19:39 +0300

    Added standard java operations specifications

--- src/main/java/org/jamplate/model/source/Document.java
+++ src/main/java/org/jamplate/model/source/Document.java
@@ -36,6 +36,40 @@ public interface Document {
 	Comparator<Document> COMPARATOR = Comparator.comparing(Document::qualifiedName);
 
 	/**
+	 * Determines if the given {@code object} equals this document or not. An object
+	 * equals a document when that object is a document and has the same {@link
+	 * #qualifiedName()} as this document. (regardless of its content, assuming the user
+	 * is honest and does not provide two documents with same qualified name but from
+	 * different origins or have different content)
+	 *
+	 * @param object the object to be matched.
+	 * @return true, if the given {@code object} equals this document.
+	 * @since 0.0.2 ~2021.01.13
+	 */
+	@Override
+	boolean equals(Object object);
+
+	/**
+	 * Calculates the hashCode of this document. The hashCode must always be the hashCode
+	 * of the {@link #qualifiedName()} of this document.
+	 *
+	 * @return the hashCode of this document.
+	 * @since 0.0.2 ~2021.01.13
+	 */
+	@Override
+	int hashCode();
+
+	/**
+	 * Returns a string representation of this document. The string representation must
+	 * always be the {@link #qualifiedName()}.
+	 *
+	 * @return a string representation of this document.
+	 * @since 0.0.2 ~2021.01.13
+	 */
+	@Override
+	String toString();
+
+	/**
 	 * Return the name of this document.
 	 *
 	 * @return the name of this document.
