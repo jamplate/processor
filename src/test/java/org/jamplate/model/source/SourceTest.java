c2f190e662fb9750922a8c7058bcc78817a4ed85
Sulaiman Oboody <lsafer@cufy.org>
2021-01-17 09:24:55 +0300

    Updated `SourceTest` to the latest updates

--- src/test/java/org/jamplate/model/source/SourceTest.java
+++ src/test/java/org/jamplate/model/source/SourceTest.java
@@ -15,17 +15,45 @@
  */
 package org.jamplate.model.source;
 
-import org.jamplate.model.sketch.AbstractSketch;
-import org.jamplate.model.sketch.Sketch;
-import org.jamplate.model.sketch.SketchVisitor;
 import org.junit.Test;
+
+import java.util.Objects;
+import java.util.regex.Pattern;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertSame;
 
 @SuppressWarnings({"MigrateAssertToMatcherAssert", "JUnitTestNG"})
 public class SourceTest {
-	static void assertRelation(Source<?> source, Source<?> other, Relation relation) {
+	static void assertCount(Sketch sketch, int count) {
+		Objects.requireNonNull(sketch, "sketch");
+		int[] sCount = {0};
+		sketch.accept(s -> {
+			sCount[0]++;
+			return false;
+		});
+		assertSame(
+				sketch + " has an unexpected inner sketches count",
+				count,
+				sCount[0]
+		);
+	}
+
+	static void assertDimensions(Source source, int position, int length) {
+		Objects.requireNonNull(source, "source");
+		assertSame(
+				source + " has an unexpected position ",
+				position,
+				source.position()
+		);
+		assertSame(
+				source + " has an unexpected length ",
+				length,
+				source.length()
+		);
+	}
+
+	static void assertRelation(Source source, Source other, Source.Relation relation) {
 		assertSame(
 				"Relation of " + other + " to " + source,
 				relation,
@@ -50,13 +78,99 @@ public class SourceTest {
 	}
 
 	@Test
+	public void matcher() {
+		TestSketch sketch = new TestSketch(new SourceSlice(new PseudoDocument("(()()())")));
+
+		//These 2 loops shall be interchangeable, mergable and independent
+		while (sketch.accept(s -> {
+			Source source = s.find(Pattern.compile("[(][^()]*[)]"));
+
+			if (source != null) {
+				Sketch it = new TestSketch(source);
+				Sketch open = new TestSketch(source.slice(0, 1), true);
+				Sketch close = new TestSketch(source.slice(source.length() - 1, 1), true);
+
+				it.put(open);
+				it.put(close);
+				s.put(it);
+
+				return true;
+			}
+
+			return false;
+		}))
+			;
+		while (sketch.accept(s -> {
+			Source source = s.find(Pattern.compile("[(]"), Pattern.compile("[)]"));
+
+			if (source != null) {
+				Sketch it = new TestSketch(source);
+				Sketch open = new TestSketch(source.slice(0, 1), true);
+				Sketch close = new TestSketch(source.slice(source.length() - 1, 1), true);
+
+				it.put(open);
+				it.put(close);
+				s.put(it);
+
+				return true;
+			}
+
+			return false;
+		}))
+			;
+
+		//base sketch
+		assertCount(sketch, 13);
+		//context group
+		TestSketch px = (TestSketch) sketch.sketches.first();
+		TestSketch pxo = (TestSketch) px.sketches.first();
+		TestSketch pxc = (TestSketch) px.sketches.last();
+		assertCount(px, 12);
+		assertCount(pxo, 1);
+		assertCount(pxc, 1);
+		assertDimensions(px.source, 0, 8);
+		assertDimensions(pxo.source, 0, 1);
+		assertDimensions(pxc.source, 7, 1);
+		//first group
+		TestSketch p1 = (TestSketch) px.sketches.stream().skip(1).findFirst().get();
+		TestSketch p1o = (TestSketch) p1.sketches.first();
+		TestSketch p1c = (TestSketch) p1.sketches.last();
+		assertCount(p1, 3);
+		assertCount(p1o, 1);
+		assertCount(p1c, 1);
+		assertDimensions(p1.source, 1, 2);
+		assertDimensions(p1o.source, 1, 1);
+		assertDimensions(p1c.source, 2, 1);
+		//second group
+		TestSketch p2 = (TestSketch) px.sketches.stream().skip(2).findFirst().get();
+		TestSketch p2o = (TestSketch) p2.sketches.first();
+		TestSketch p2c = (TestSketch) p2.sketches.last();
+		assertCount(p2, 3);
+		assertCount(p2o, 1);
+		assertCount(p2c, 1);
+		assertDimensions(p2.source, 3, 2);
+		assertDimensions(p2o.source, 3, 1);
+		assertDimensions(p2c.source, 4, 1);
+		//third group
+		TestSketch p3 = (TestSketch) px.sketches.stream().skip(3).findFirst().get();
+		TestSketch p3o = (TestSketch) p3.sketches.first();
+		TestSketch p3c = (TestSketch) p3.sketches.last();
+		assertCount(p3, 3);
+		assertCount(p3o, 1);
+		assertCount(p3c, 1);
+		assertDimensions(p3.source, 5, 2);
+		assertDimensions(p3o.source, 5, 1);
+		assertDimensions(p3c.source, 6, 1);
+	}
+
+	@Test
 	public void relations() {
-		Source<?> source = new PseudoSource<>("pseudo", "ABC0123", 0);
-		Source<?> letters = source.slice(0, 3);
-		Source<?> numbers = source.slice(3, 4);
-		Source<?> b = source.slice(1, 1);
-		Source<?> bc = letters.slice(1, 2);
-		Source<?> c0 = source.slice(2, 2);
+		Source source = new SourceSlice(new PseudoDocument("ABC0123"));
+		Source letters = source.slice(0, 3);
+		Source numbers = source.slice(3, 4);
+		Source b = source.slice(1, 1);
+		Source bc = letters.slice(1, 2);
+		Source c0 = source.slice(2, 2);
 
 		assertEquals("Wrong Slice", "ABC", letters.content());
 		assertEquals("Wrong Slice", "0123", numbers.content());
@@ -64,78 +178,61 @@ public class SourceTest {
 		assertEquals("Wrong Slice", "BC", bc.content());
 		assertEquals("Wrong Slice", "C0", c0.content());
 
-		assertRelation(source, source, Relation.SAME);
-		assertRelation(source, letters, Relation.START);
-		assertRelation(source, numbers, Relation.END);
-		assertRelation(source, b, Relation.FRAGMENT);
-		assertRelation(source, bc, Relation.FRAGMENT);
-		assertRelation(source, c0, Relation.FRAGMENT);
+		assertRelation(source, source, Source.Relation.SAME);
+		assertRelation(source, letters, Source.Relation.START);
+		assertRelation(source, numbers, Source.Relation.END);
+		assertRelation(source, b, Source.Relation.FRAGMENT);
+		assertRelation(source, bc, Source.Relation.FRAGMENT);
+		assertRelation(source, c0, Source.Relation.FRAGMENT);
 
-		assertRelation(letters, letters, Relation.SAME);
-		assertRelation(letters, numbers, Relation.NEXT);
-		assertRelation(letters, b, Relation.FRAGMENT);
-		assertRelation(letters, bc, Relation.END);
-		assertRelation(letters, c0, Relation.OVERFLOW);
+		assertRelation(letters, letters, Source.Relation.SAME);
+		assertRelation(letters, numbers, Source.Relation.NEXT);
+		assertRelation(letters, b, Source.Relation.FRAGMENT);
+		assertRelation(letters, bc, Source.Relation.END);
+		assertRelation(letters, c0, Source.Relation.OVERFLOW);
 
-		assertRelation(numbers, numbers, Relation.SAME);
-		assertRelation(numbers, b, Relation.BEFORE);
-		assertRelation(numbers, bc, Relation.PREVIOUS);
-		assertRelation(numbers, c0, Relation.UNDERFLOW);
+		assertRelation(numbers, numbers, Source.Relation.SAME);
+		assertRelation(numbers, b, Source.Relation.BEFORE);
+		assertRelation(numbers, bc, Source.Relation.PREVIOUS);
+		assertRelation(numbers, c0, Source.Relation.UNDERFLOW);
 
-		assertRelation(b, b, Relation.SAME);
-		assertRelation(b, bc, Relation.AHEAD);
-		assertRelation(b, c0, Relation.NEXT);
+		assertRelation(b, b, Source.Relation.SAME);
+		assertRelation(b, bc, Source.Relation.AHEAD);
+		assertRelation(b, c0, Source.Relation.NEXT);
 
-		assertRelation(bc, bc, Relation.SAME);
-		assertRelation(bc, c0, Relation.OVERFLOW);
+		assertRelation(bc, bc, Source.Relation.SAME);
+		assertRelation(bc, c0, Source.Relation.OVERFLOW);
 
-		assertRelation(c0, c0, Relation.SAME);
+		assertRelation(c0, c0, Source.Relation.SAME);
 	}
 
-	@Test
-	public void visit() {
-		//		Set<Source> sub = source.find(pattern);
-		//		Set<Set<Source>> subsub = sub.stream()
-		//				.map(s -> (Set<Source>) s.find(pattern))
-		//				.collect(Collectors.toSet());
-		//		sub.forEach(s -> System.out.println(s.content()));
-		//		subsub.forEach(set -> set.forEach(s -> System.out.println(s.content())));
-		//
-		//		Set<Source> sub = source.find(Pattern.compile("[(]"), Pattern.compile("[)]"));
-		//		sub.forEach(s -> System.out.println(s.content()));
-		//		Pattern pattern = Pattern.compile("[(].*[)]");
-		Source source = new PseudoSource<>("pseudo", "((A), (B), (C))", 0);
-		SketchImpl sketch = new SketchImpl(source);
-		sketch.put(new SketchImpl(source.slice(1, 3)));
-		sketch.put(new SketchImpl(source.slice(6, 3)));
-		sketch.put(new SketchImpl(source.slice(11, 3)));
+	public static class TestSketch extends AbstractSketch {
+		private final boolean reserved;
 
-		sketch.put(new SketchImpl(source.slice(7, 1)));
-		sketch.put(new SketchImpl(source.slice(2, 1)));
-		sketch.put(new SketchImpl(source.slice(12, 1)));
-
-		sketch.accept(new SketchVisitor() {
-			@Override
-			public boolean visitSketch(Sketch sketch) {
-				System.out.println(sketch + " ~ " + sketch.source().content());
-				return false;
-			}
-
-			@Override
-			public boolean visitSource(Sketch sketch, Source source) {
-				System.out.println(sketch + " found " + source + " ~ " + source.content());
-				return source.content().equals("C");
-			}
-		});
-	}
-
-	public static class SketchImpl extends AbstractSketch {
-		public SketchImpl(Source source) {
-			super(source);
+		public TestSketch(Source source) {
+			this(source, false);
 		}
 
-		public void add(int pos, int len) {
-			this.sketches.add(new SketchImpl(this.source.slice(pos, len)));
+		public TestSketch(Source source, boolean reserved) {
+			super(source);
+			this.reserved = reserved;
+		}
+
+		@Override
+		public Source find(Pattern pattern) {
+			return this.reserved ? null : super.find(pattern);
+		}
+
+		@Override
+		public Source find(Pattern startPattern, Pattern endPattern) {
+			return this.reserved ? null : super.find(startPattern, endPattern);
+		}
+
+		@Override
+		public void put(Sketch sketch) {
+			if (this.reserved)
+				throw new UnsupportedOperationException();
+			super.put(sketch);
 		}
 	}
 }
