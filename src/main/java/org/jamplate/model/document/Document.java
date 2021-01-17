2e4b757c2dfc8679b8f7a1bd075e109e9d9f4d04
Sulaiman Oboody <lsafer@cufy.org>
2021-01-17 15:33:40 +0300

    Moved anything todo with `interface Document` to `package document`
    - Moved `interface Document`, `class FileDocument` and `class PseudoDocument` to package `org.jamplate.model.document` ~ to organize document classes more easily.

--- src/main/java/org/jamplate/model/document/Document.java
+++ src/main/java/org/jamplate/model/document/Document.java
@@ -13,7 +13,7 @@
  *	See the License for the specific language governing permissions and
  *	limitations under the License.
  */
-package org.jamplate.model.source;
+package org.jamplate.model.document;
 
 import java.io.IOError;
 import java.io.IOException;
