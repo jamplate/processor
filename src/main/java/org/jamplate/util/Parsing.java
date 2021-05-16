da90a2cc20cf78e9716434b003fc798aef8acc46
Sulaiman Oboody <lsafer@cufy.org>
2021-05-16 05:16:56 +0300

    Moved `class Parsing` to `package org.jamplate.util`

--- src/main/java/org/jamplate/util/Parsing.java
+++ src/main/java/org/jamplate/util/Parsing.java
@@ -13,7 +13,7 @@
  *	See the License for the specific language governing permissions and
  *	limitations under the License.
  */
-package org.jamplate.compile;
+package org.jamplate.util;
 
 import org.jamplate.model.*;
 import org.jetbrains.annotations.Contract;
