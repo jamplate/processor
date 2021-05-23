68366583d63ae74d038f747729b4f8cec2880dad
Sulaiman Oboody <lsafer@cufy.org>
2021-05-23 14:58:47 +0300

    Updated class `Memory`
    - added class `Memory.Frame`.

--- src/main/java/org/jamplate/model/Memory.java
+++ src/main/java/org/jamplate/model/Memory.java
@@ -17,9 +17,14 @@ package org.jamplate.model;
 
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
+import java.io.Closeable;
+import java.io.IOError;
+import java.io.IOException;
 import java.util.*;
 import java.util.function.UnaryOperator;
+import java.util.stream.Collectors;
 
 /**
  * The memory is a structure that saves the variables at the runtime.
@@ -28,29 +33,22 @@ import java.util.function.UnaryOperator;
  * @version 0.2.0
  * @since 0.2.0 ~2021.05.21
  */
-public final class Memory {
+public final class Memory implements Closeable {
 	/**
-	 * A buffer containing the text considered printed to the console.
+	 * The frames in this memory. Always not empty.
 	 *
-	 * @since 0.2.0 ~2021.05.21
-	 */
-	@SuppressWarnings("StringBufferField")
-	@NotNull
-	private final StringBuilder console = new StringBuilder();
-	/**
-	 * A map backing the heap part of this memory.
-	 *
-	 * @since 0.2.0 ~2021.05.21
+	 * @since 0.2.0 ~2021.05.23
 	 */
 	@NotNull
-	private final Map<String, Value> heap = new HashMap<>();
+	private final Deque<Frame> frames;
+
 	/**
-	 * A queue backing the stack part of this memory.
+	 * A an appendable to append the text printed to the console to it.
 	 *
 	 * @since 0.2.0 ~2021.05.21
 	 */
 	@NotNull
-	private final Queue<Queue<Value>> stack = new LinkedList<>(Collections.singleton(new LinkedList<>()));
+	private Appendable console;
 
 	/**
 	 * Construct a new empty memory.
@@ -58,23 +56,30 @@ public final class Memory {
 	 * @since 0.2.0 ~2021.05.21
 	 */
 	public Memory() {
+		this.frames = new LinkedList<>(Collections.singleton(new Frame()));
+		this.console = new StringBuilder();
 	}
 
 	/**
-	 * Construct a copy of the given {@code memory}.
+	 * Construct a new copy of the given {@code memory}.
 	 *
 	 * @param memory the memory to be copied.
 	 * @throws NullPointerException if the given {@code memory} is null.
-	 * @since 0.2.0 ~2021.05.21
+	 * @since 0.2.0 ~2021.05.23
 	 */
 	public Memory(@NotNull Memory memory) {
 		Objects.requireNonNull(memory, "memory");
-		this.console.append(memory.console);
-		this.heap.putAll(memory.heap);
-		memory.stack
+		this.frames = memory.frames
 				.stream()
-				.map(LinkedList::new)
-				.forEach(this.stack::add);
+				.map(Frame::new)
+				.collect(Collectors.toCollection(LinkedList::new));
+		this.console = memory.console;
+	}
+
+	@Override
+	public void close() throws IOException {
+		if (this.console instanceof Closeable)
+			((Closeable) this.console).close();
 	}
 
 	/**
@@ -83,22 +88,67 @@ public final class Memory {
 	 * <br>
 	 * If the given {@code operator} thrown any exception when invoked by this method. The
 	 * thrown exception will fall throw this method with nothing changed.
+	 * <br>
+	 * The computing will be performed on the last frame that contain an allocation to the
+	 * given {@code address}. If no frame has an allocation to the given {@code address}
+	 * then the result of the computation will be store in the base frame.
 	 *
 	 * @param address  the address to replace the value at.
 	 * @param operator the function to be invoked to acquire the replacement value.
+	 * @return the computed value.
 	 * @throws NullPointerException if the given {@code address} or {@code operator} is
 	 *                              null.
 	 * @since 0.2.0 ~2021.05.21
 	 */
 	@Contract(mutates = "this")
-	public void compute(@NotNull String address, @NotNull UnaryOperator<Value> operator) {
+	public Value compute(@NotNull String address, @NotNull UnaryOperator<Value> operator) {
 		Objects.requireNonNull(address, "address");
 		Objects.requireNonNull(operator, "operator");
-		this.heap.compute(address, (k, v) -> operator.apply(v));
+		Iterator<Frame> iterator = this.frames.descendingIterator();
+		while (iterator.hasNext()) {
+			Frame frame = iterator.next();
+			//noinspection AccessingNonPublicFieldOfAnotherObject
+			Value value = frame.heap.getOrDefault(address, null);
+
+			if (value != null) {
+				Value v = operator.apply(value);
+				frame.set(address, v);
+				return v;
+			}
+		}
+
+		Value v = operator.apply(Value.NULL);
+		this.frames.getFirst().set(address, v);
+		return v;
+	}
+
+	/**
+	 * Dump the stack and the heap of the current frame to frame before it. If the current
+	 * frame is the base frame, then a copy of the base from will be returned.
+	 *
+	 * @return the dumped frame.
+	 * @since 0.2.0 ~2021.05.23
+	 */
+	@NotNull
+	@Contract(mutates = "this")
+	public Frame dumpFrame() {
+		if (this.frames.size() == 1)
+			return new Frame(this.frames.peek());
+
+		Frame frame = this.frames.pollLast();
+		Frame before = this.frames.peekLast();
+		//noinspection AccessingNonPublicFieldOfAnotherObject
+		before.stack.addAll(frame.stack);
+		//noinspection AccessingNonPublicFieldOfAnotherObject
+		before.heap.putAll(frame.heap);
+		return frame;
 	}
 
 	/**
 	 * Return the value allocated at the given {@code address} in this memory.
+	 * <br>
+	 * The value is returned from the last frame that has an allocation in the given
+	 * {@code address}.
 	 *
 	 * @param address the address to be read.
 	 * @return the value at the given {@code address}. Or {@link Value#NULL} if no value
@@ -110,11 +160,59 @@ public final class Memory {
 	@Contract(pure = true)
 	public Value get(@NotNull String address) {
 		Objects.requireNonNull(address, "address");
-		return this.heap.getOrDefault(address, Value.NULL);
+		Iterator<Frame> iterator = this.frames.descendingIterator();
+		while (iterator.hasNext()) {
+			//noinspection AccessingNonPublicFieldOfAnotherObject
+			Value value = iterator.next().heap.getOrDefault(address, null);
+
+			if (value != null)
+				return value;
+		}
+
+		return Value.NULL;
+	}
+
+	/**
+	 * Return current set console appendable.
+	 *
+	 * @return the current console.
+	 * @since 0.2.0 ~2021.05.23
+	 */
+	@NotNull
+	@Contract(pure = true)
+	public Appendable getConsole() {
+		return this.console;
+	}
+
+	/**
+	 * Return the current last frame.
+	 *
+	 * @return the current frame.
+	 * @since 0.2.0 ~2021.05.23
+	 */
+	@NotNull
+	@Contract(pure = true)
+	public Frame getFrame() {
+		return this.frames.getLast();
+	}
+
+	/**
+	 * Return an array containing the current frames in this memory.
+	 *
+	 * @return an array of the current frames.
+	 * @since 0.2.0 ~2021.05.23
+	 */
+	@NotNull
+	@Contract(value = "->new", pure = true)
+	public Frame[] getFrames() {
+		//noinspection SimplifyStreamApiCallChains
+		return this.frames.stream().toArray(Frame[]::new);
 	}
 
 	/**
 	 * Return and remove the last pushed value to the stack.
+	 * <br>
+	 * The stack of the last frame will be modified.
 	 *
 	 * @return the last value pushed to the stack. Or {@link Value#NULL} if the stack was
 	 * 		empty.
@@ -123,23 +221,25 @@ public final class Memory {
 	@NotNull
 	@Contract(mutates = "this")
 	public Value pop() {
-		Value value = this.stack.peek().poll();
-		return value == null ? Value.NULL : value;
+		return this.frames.getLast().pop();
 	}
 
 	/**
 	 * Pop the current stack frame. Popping the stack frame will result to this memory
 	 * representing the frame previous to the last pushed frame. If the base frame to be
-	 * popped, then the base frame will be cleared instead of popped.
+	 * popped, then the base frame will be replaced with a new one instead of popped.
 	 *
+	 * @return the popped frame.
 	 * @since 0.2.0 ~2021.05.21
 	 */
 	@Contract(mutates = "this")
-	public void popFrame() {
-		if (this.stack.size() == 1)
-			this.stack.peek().clear();
-		else
-			this.stack.poll();
+	public Frame popFrame() {
+		Frame frame = this.frames.pollLast();
+
+		if (this.frames.isEmpty())
+			this.frames.addLast(new Frame());
+
+		return frame;
 	}
 
 	/**
@@ -150,28 +250,23 @@ public final class Memory {
 	 *
 	 * @param text the text to be printed.
 	 * @throws NullPointerException if the given {@code text} is null.
+	 * @throws IOError              if an error occurred while writing to the console.
 	 * @since 0.2.0 ~2021.05.21
 	 */
 	@Contract(mutates = "this")
 	public void print(@NotNull CharSequence text) {
 		Objects.requireNonNull(text, "text");
-		this.console.append(text);
-	}
-
-	/**
-	 * Read the contents printed to the console.
-	 *
-	 * @return the contents printed to the console.
-	 * @since 0.2.0 ~2021.05.21
-	 */
-	@NotNull
-	@Contract(pure = true)
-	public String read() {
-		return this.console.toString();
+		try {
+			this.console.append(text);
+		} catch (IOException e) {
+			throw new IOError(e);
+		}
 	}
 
 	/**
 	 * Push the given {@code value} to the stack part of this memory.
+	 * <br>
+	 * The value will be pushed to the stack of the last frame.
 	 *
 	 * @param value the value to be pushed.
 	 * @throws NullPointerException if the given {@code value} is null.
@@ -180,21 +275,36 @@ public final class Memory {
 	@Contract(mutates = "this")
 	public void push(@NotNull Value value) {
 		Objects.requireNonNull(value, "value");
-		this.stack.peek().add(value);
+		this.frames.getLast().push(value);
 	}
 
 	/**
-	 * Push a new stack frame.
+	 * Push a new frame.
 	 *
-	 * @since 0.2.0 ~2021.05.21
+	 * @since 0.2.0 ~2021.05.23
 	 */
 	@Contract(mutates = "this")
 	public void pushFrame() {
-		this.stack.add(new LinkedList<>());
+		this.frames.addLast(new Frame());
+	}
+
+	/**
+	 * Push the given {@code frame} in this memory.
+	 *
+	 * @param frame the frame to be pushed.
+	 * @throws NullPointerException if the given {@code frame} is null.
+	 * @since 0.2.0 ~2021.05.21
+	 */
+	@Contract(mutates = "this")
+	public void pushFrame(@NotNull Frame frame) {
+		Objects.requireNonNull(frame, "frame");
+		this.frames.addLast(frame);
 	}
 
 	/**
 	 * Set the value at the given {@code address} to be the given {@code value}.
+	 * <br>
+	 * The value will be allocated at the heap of the first frame.
 	 *
 	 * @param address the address to set the value to.
 	 * @param value   the value to be set at the given {@code address}.
@@ -206,6 +316,201 @@ public final class Memory {
 	public void set(@NotNull String address, @NotNull Value value) {
 		Objects.requireNonNull(address, "address");
 		Objects.requireNonNull(value, "value");
-		this.heap.put(address, value);
+		this.frames.getFirst().set(address, value);
+	}
+
+	/**
+	 * Set the console to be the given {@code console}.
+	 *
+	 * @param console the new console to be set.
+	 * @throws NullPointerException if the given {@code console} is null.
+	 * @since 0.2.0 ~2021.05.23
+	 */
+	@Contract(mutates = "this")
+	public void setConsole(@NotNull Appendable console) {
+		Objects.requireNonNull(console, "console");
+		if (this.console instanceof Closeable)
+			try {
+				((Closeable) this.console).close();
+			} catch (IOException e) {
+				//noinspection CallToPrintStackTrace
+				e.printStackTrace();
+			}
+
+		this.console = console;
+	}
+
+	/**
+	 * An isolated part in the memory.
+	 *
+	 * @author LSafer
+	 * @version 0.2.0
+	 * @since 0.2.0 ~2021.05.23
+	 */
+	public static final class Frame {
+		/**
+		 * The heap part of this frame.
+		 *
+		 * @since 0.2.0 ~2021.05.23
+		 */
+		@NotNull
+		private final Map<String, Value> heap;
+		/**
+		 * The stack part of this frame.
+		 *
+		 * @since 0.2.0 ~2021.05.23
+		 */
+		@NotNull
+		private final Queue<Value> stack;
+
+		/**
+		 * The instruction of this frame.
+		 *
+		 * @since 0.2.0 ~2021.05.23
+		 */
+		@Nullable
+		private Instruction instruction;
+
+		/**
+		 * Construct a new frame.
+		 *
+		 * @since 0.2.0 ~2021.05.23
+		 */
+		public Frame() {
+			this.heap = new HashMap<>();
+			this.stack = new LinkedList<>();
+		}
+
+		/**
+		 * Construct a copy of the given {@code frame}.
+		 *
+		 * @param frame the frame to copy.
+		 * @throws NullPointerException if the given {@code frame} is null.
+		 * @since 0.2.0 ~2021.05.23
+		 */
+		public Frame(@NotNull Frame frame) {
+			Objects.requireNonNull(frame, "frame");
+			this.heap = new HashMap<>(frame.heap);
+			this.stack = new LinkedList<>();
+			this.instruction = frame.instruction;
+		}
+
+		/**
+		 * Construct a new frame with the given {@code instruction}.
+		 *
+		 * @param instruction the instruction the constructed frame is for.
+		 * @throws NullPointerException if the given {@code instruction} is null.
+		 * @since 0.2.0 ~2021.05.23
+		 */
+		public Frame(@NotNull Instruction instruction) {
+			Objects.requireNonNull(instruction, "instruction");
+			this.heap = new HashMap<>();
+			this.stack = new LinkedList<>();
+			this.instruction = instruction;
+		}
+
+		/**
+		 * Replace the value at the given {@code address} with the result of invoking the
+		 * given {@code operator} with the current value.
+		 * <br>
+		 * If the given {@code operator} thrown any exception when invoked by this method.
+		 * The thrown exception will fall throw this method with nothing changed.
+		 *
+		 * @param address  the address to replace the value at.
+		 * @param operator the function to be invoked to acquire the replacement value.
+		 * @return the computed value.
+		 * @throws NullPointerException if the given {@code address} or {@code operator}
+		 *                              is null.
+		 * @since 0.2.0 ~2021.05.23
+		 */
+		@Contract(mutates = "this")
+		public Value compute(@NotNull String address, @NotNull UnaryOperator<Value> operator) {
+			Objects.requireNonNull(address, "address");
+			Objects.requireNonNull(operator, "operator");
+			return this.heap.compute(address, (k, v) -> operator.apply(v));
+		}
+
+		/**
+		 * Return the value allocated at the given {@code address} in this memory.
+		 *
+		 * @param address the address to be read.
+		 * @return the value at the given {@code address}. Or {@link Value#NULL} if no
+		 * 		value was allocated there.
+		 * @throws NullPointerException if the given {@code address} is null.
+		 * @since 0.2.0 ~2021.05.23
+		 */
+		@NotNull
+		@Contract(pure = true)
+		public Value get(@NotNull String address) {
+			Objects.requireNonNull(address, "address");
+			return this.heap.getOrDefault(address, Value.NULL);
+		}
+
+		/**
+		 * Return the instruction of this frame.
+		 *
+		 * @return the instruction of this frame.
+		 * @since 0.2.0 ~2021.05.23
+		 */
+		@Nullable
+		@Contract(pure = true)
+		public Instruction getInstruction() {
+			return this.instruction;
+		}
+
+		/**
+		 * Return and remove the last pushed value to the stack.
+		 *
+		 * @return the last value pushed to the stack. Or {@link Value#NULL} if the stack
+		 * 		was empty.
+		 * @since 0.2.0 ~2021.05.23
+		 */
+		@NotNull
+		@Contract(mutates = "this")
+		public Value pop() {
+			Value value = this.stack.poll();
+			return value == null ? Value.NULL : value;
+		}
+
+		/**
+		 * Push the given {@code value} to the stack part of this memory.
+		 *
+		 * @param value the value to be pushed.
+		 * @throws NullPointerException if the given {@code value} is null.
+		 * @since 0.2.0 ~2021.05.23
+		 */
+		@Contract(mutates = "this")
+		public void push(@NotNull Value value) {
+			Objects.requireNonNull(value, "value");
+			this.stack.add(value);
+		}
+
+		/**
+		 * Set the value at the given {@code address} to be the given {@code value}.
+		 *
+		 * @param address the address to set the value to.
+		 * @param value   the value to be set at the given {@code address}.
+		 * @throws NullPointerException if the given {@code address} or {@code value} is
+		 *                              null.
+		 * @since 0.2.0 ~2021.05.23
+		 */
+		@Contract(mutates = "this")
+		public void set(@NotNull String address, @NotNull Value value) {
+			Objects.requireNonNull(address, "address");
+			Objects.requireNonNull(value, "value");
+			this.heap.put(address, value);
+		}
+
+		/**
+		 * Set the instruction of this frame to be the given {@code instruction}.
+		 *
+		 * @param instruction the instruction of this frame.
+		 * @since 0.2.0 ~2021.05.23
+		 */
+		@Contract(mutates = "this")
+		public void setInstruction(@NotNull Instruction instruction) {
+			Objects.requireNonNull(instruction, "instruction");
+			this.instruction = instruction;
+		}
 	}
 }
