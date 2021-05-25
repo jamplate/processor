982d372ab8eca7ba239496fec61f6de12773c411
Sulaiman Oboody <lsafer@cufy.org>
2021-05-25 14:16:35 +0300

    Moved parsers from `org.jamplate.impl.model.function`
    - Moved class `LiteralParser` to package `org.jamplate.impl.parsers`.
    - Moved class `ScopeParser` to package `org.jamplate.impl.parsers`.

--- src/main/java/org/jamplate/impl/parser/ScopeParser.java
+++ src/main/java/org/jamplate/impl/parser/ScopeParser.java
@@ -13,7 +13,7 @@
  *	See the License for the specific language governing permissions and
  *	limitations under the License.
  */
-package org.jamplate.impl.util.model.function;
+package org.jamplate.impl.parser;
 
 import org.jamplate.impl.util.model.ScopeSketch;
 import org.jamplate.model.Compilation;
