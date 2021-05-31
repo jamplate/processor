c986007bf88be3d7ec686d6d2ac86a0a79b48e34
Sulaiman Oboody <lsafer@cufy.org>
2021-05-31 20:02:14 +0300

    Updated interface `Diagnostic`
    - now implements `Iterable<Message>`.
    - now implements `Serializable`.
    - added method `flush()`.
    - added method `clear()`.
    - added method `flush(PrintStream, PrintStream)`.
    - added method `format(Message)`.
    - added method `print(Message)`.

--- src/main/java/org/jamplate/diagnostic/Diagnostic.java
+++ src/main/java/org/jamplate/diagnostic/Diagnostic.java
@@ -15,6 +15,12 @@
  */
 package org.jamplate.diagnostic;
 
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.PrintStream;
+import java.io.Serializable;
+
 /**
  * An interface to store and print notes, warnings and errors to the console.
  *
@@ -22,5 +28,64 @@ package org.jamplate.diagnostic;
  * @version 0.2.0
  * @since 0.2.0 ~2021.05.16
  */
-public interface Diagnostic {
+public interface Diagnostic extends Iterable<Message>, Serializable {
+	/**
+	 * Flush the printed messages to the default print stream.
+	 *
+	 * @return this.
+	 * @since 0.2.0 ~2021.05.31
+	 */
+	@NotNull
+	@Contract(value = "->this", mutates = "this")
+	default Diagnostic flush() {
+		//noinspection UseOfSystemOutOrSystemErr
+		return this.flush(System.out, System.err);
+	}
+
+	/**
+	 * Clear the message queue.
+	 *
+	 * @return this.
+	 * @since 0.2.0 ~2021.05.31
+	 */
+	@NotNull
+	@Contract(value = "->this", mutates = "this")
+	Diagnostic clear();
+
+	/**
+	 * Flush the printed messages to the default print stream.
+	 *
+	 * @param out the default output stream.
+	 * @param err the error stream.
+	 * @return this.
+	 * @since 0.2.0 ~2021.05.31
+	 */
+	@NotNull
+	@Contract(value = "_,_->this", mutates = "this,param1,param2")
+	Diagnostic flush(PrintStream out, PrintStream err);
+
+	/**
+	 * Format the given {@code message}.
+	 *
+	 * @param message the message to be formatted.
+	 * @return a string representation of the given {@code message} in the style of this
+	 * 		diagnostic system.
+	 * @throws NullPointerException if the given {@code message} is null.
+	 * @since 0.2.0 ~2021.05.31
+	 */
+	@NotNull
+	@Contract(value = "_->new", pure = true)
+	String format(@NotNull Message message);
+
+	/**
+	 * Print the given {@code message}.
+	 *
+	 * @param message the message to be printed.
+	 * @return this.
+	 * @throws NullPointerException if the given {@code message} is null.
+	 * @since 0.2.0 ~2021.05.31
+	 */
+	@NotNull
+	@Contract(value = "_->this", mutates = "this")
+	Diagnostic print(@NotNull Message message);
 }
