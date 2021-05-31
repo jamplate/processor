b6e4d1c4ae39423a1fc3fee42fa957d32f9ddf81
Sulaiman Oboody <lsafer@cufy.org>
2021-05-31 19:59:33 +0300

    Updated class `Message`
    - now is a `Serializable`.
    - added method `Message.getMessagePhrase()`.
    - added method `Message.getException()`.
    - added method `Message.getPriority()`.
    - added method `Message.getErrorKind()`.
    - added method `Message.isFetal()`.
    - added method `Message.getCriticalPoints()`.
    - added method `Message.getStackTrace()`.

--- src/main/java/org/jamplate/diagnostic/Message.java
+++ src/main/java/org/jamplate/diagnostic/Message.java
@@ -15,6 +15,13 @@
  */
 package org.jamplate.diagnostic;
 
+import org.jamplate.model.Tree;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.Serializable;
+
 /**
  * A diagnostic message containing the details about an error, warning or even a note.
  *
@@ -22,5 +29,74 @@ package org.jamplate.diagnostic;
  * @version 0.2.0
  * @since 0.2.0 ~2021.05.17
  */
-public interface Message {
+public interface Message extends Serializable {
+	/**
+	 * Return the critical points trees that caused the error represented by this
+	 * message.
+	 *
+	 * @return a clone array of the critical points trees. Or an empty array if none.
+	 * @since 0.2.0 ~2021.05.31
+	 */
+	@NotNull
+	@Contract(value = "->new", pure = true)
+	Tree @NotNull [] getCriticalPoints();
+
+	/**
+	 * Return the kind of the error of this message.
+	 *
+	 * @return the kind of the error.
+	 * @since 0.2.0 ~2021.05.31
+	 */
+	@NotNull
+	@Contract(pure = true)
+	String getErrorKind();
+
+	/**
+	 * Return the exception caused this message.
+	 *
+	 * @return the exception of this message. Or {@code null} if none.
+	 * @since 0.2.0 ~2021.05.31
+	 */
+	@Nullable
+	@Contract(pure = true)
+	Throwable getException();
+
+	/**
+	 * Return the message phrase.
+	 *
+	 * @return the message phrase.
+	 * @since 0.2.0 ~2021.05.31
+	 */
+	@NotNull
+	@Contract(pure = true)
+	String getMessagePhrase();
+
+	/**
+	 * Return the priority of this message.
+	 *
+	 * @return the priority of this message.
+	 * @since 0.2.0 ~2021.05.31
+	 */
+	@NotNull
+	@Contract(pure = true)
+	String getPriority();
+
+	/**
+	 * Return the trace of the error.
+	 *
+	 * @return the trace of the error, or an empty array if none.
+	 * @since 0.2.0 ~2021.05.31
+	 */
+	@NotNull
+	@Contract(value = "->new", pure = true)
+	Tree @NotNull [] getStackTrace();
+
+	/**
+	 * Return {@code true} if this message represent a fetal (unrecoverable) error.
+	 *
+	 * @return true, if this message represents a fetal error.
+	 * @since 0.2.0 ~2021.05.31
+	 */
+	@Contract(pure = true)
+	boolean isFetal();
 }
