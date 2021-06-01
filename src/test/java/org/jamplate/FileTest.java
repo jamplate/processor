88786fbd072b447795d92e4615493f2a617d197f
Sulaiman Oboody <lsafer@cufy.org>
2021-06-01 22:12:15 +0300

    tests

--- src/test/java/org/jamplate/FileTest.java
+++ src/test/java/org/jamplate/FileTest.java
@@ -5,6 +5,7 @@ import org.jamplate.impl.Address;
 import org.jamplate.impl.Jamplate;
 import org.jamplate.impl.model.EnvironmentImpl;
 import org.jamplate.impl.model.FileDocument;
+import org.jamplate.impl.model.PseudoDocument;
 import org.jamplate.model.Compilation;
 import org.jamplate.model.Document;
 import org.jamplate.model.Environment;
@@ -13,6 +14,53 @@ import org.junit.jupiter.api.Test;
 import java.io.File;
 
 public class FileTest {
+	@SuppressWarnings("JUnitTestMethodWithNoAssertions")
+	@Test
+	public void pseudo() {
+		Document[] documents = {
+				new PseudoDocument(
+						"index.jamplate",
+						""
+				)
+		};
+
+		Environment environment = new EnvironmentImpl();
+
+		environment.getMeta().put(Address.PROJECT, new File("test_input"));
+
+		boolean compiled = Jamplate.compile(environment, documents);
+
+		if (!compiled) {
+			System.err.println("Compilation Error");
+			System.err.println();
+			environment.getDiagnostic()
+					   .flush(true);
+			return;
+		}
+
+		Compilation[] jamplates = environment
+				.compilationSet()
+				.stream()
+				.filter(compilation -> compilation.getRootTree().document().toString().endsWith(".jamplate"))
+				.toArray(Compilation[]::new);
+
+		boolean executed = Jamplate.execute(environment, jamplates);
+
+		if (!executed) {
+			System.err.println("Runtime Error");
+			System.err.println();
+			environment.getDiagnostic()
+					   .flush(true);
+			return;
+		}
+
+		environment.getDiagnostic()
+				   .flush();
+
+		System.out.println();
+		System.out.println("Jamplate ended successfully");
+	}
+
 	@SuppressWarnings("JUnitTestMethodWithNoAssertions")
 	@Test
 	public void run() {
@@ -29,6 +77,7 @@ public class FileTest {
 			System.err.println();
 			environment.getDiagnostic()
 					   .flush(true);
+			return;
 		}
 
 		Compilation[] jamplates = environment
@@ -44,6 +93,7 @@ public class FileTest {
 			System.err.println();
 			environment.getDiagnostic()
 					   .flush(true);
+			return;
 		}
 
 		environment.getDiagnostic()
