b97afaf967ee18c16d3cba5a08276c5567173263
Sulaiman Oboody <lsafer@cufy.org>
2021-07-10 04:22:34 +0300

    Updated README.md

---
+++ src/test/groovy/org/jamplate/glucose/GlucoseGroovyTest.groovy
@@ -0,0 +1,57 @@
+package org.jamplate.glucose
+
+import org.jamplate.api.Unit
+import org.jamplate.glucose.spec.GlucoseSpec
+import org.jamplate.glucose.spec.tool.DebugSpec
+import org.jamplate.impl.api.UnitImpl
+import org.jamplate.impl.model.PseudoDocument
+import org.jamplate.model.Document
+import org.junit.jupiter.api.Test
+
+class GlucoseGroovyTest {
+	@Test
+	void example0() {
+		Document document = new PseudoDocument(
+				//name
+				"main.jamplate",
+				//content
+				"#message Hello ' ' World"
+		)
+
+		Unit unit = new UnitImpl(new GlucoseSpec())
+
+		if (
+		!unit.initialize(document) ||
+		!unit.parse(document) ||
+		!unit.analyze(document) ||
+		!unit.compile(document) ||
+		!unit.execute(document)
+		) {
+			unit.diagnostic()
+		}
+	}
+
+	@Test
+	void example1() {
+		Document document = new PseudoDocument(
+				//name
+				"main.jamplate",
+				//content
+				"#{ Hello ' ' World }#"
+		)
+
+		Unit unit = new UnitImpl(new GlucoseSpec(
+				DebugSpec.INSTANCE
+		))
+
+		if (
+		!unit.initialize(document) ||
+		!unit.parse(document) ||
+		!unit.analyze(document) ||
+		!unit.compile(document) ||
+		!unit.execute(document)
+		) {
+			unit.diagnostic()
+		}
+	}
+}
