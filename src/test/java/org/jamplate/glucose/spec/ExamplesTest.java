04bca91152c8c874b8a9d92da22a8ee2fdee776c
Sulaiman Oboody <lsafer@cufy.org>
2021-07-11 22:25:18 +0300

    tests

--- src/test/java/org/jamplate/glucose/spec/ExamplesTest.java
+++ src/test/java/org/jamplate/glucose/spec/ExamplesTest.java
@@ -2,16 +2,25 @@ package org.jamplate.glucose.spec;
 
 import org.jamplate.glucose.spec.tool.DebugSpec;
 import org.jamplate.impl.document.PseudoDocument;
+import org.jamplate.impl.unit.Action;
 import org.jamplate.impl.unit.UnitImpl;
 import org.jamplate.model.Document;
 import org.jamplate.unit.Unit;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+import static org.jamplate.glucose.internal.util.Values.number;
+import static org.jamplate.util.Specs.listener;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class ExamplesTest {
 	@Test
 	public void _99bottlesOfBeerOnTheWall() {
+		//manual test :)
 		Document document = new PseudoDocument(
 				"Test",
 				"#declare $i 99",
@@ -54,5 +63,120 @@ public class ExamplesTest {
 			unit.diagnostic();
 			fail("Test not completed");
 		}
+	}
+
+	@Test
+	public void _fibonacci() {
+		for (
+				int[] arr : new int[][]{
+				{0, 0},
+				{1, 1},
+				{2, 1},
+				{3, 2},
+				{4, 3},
+				{5, 5},
+				{6, 8},
+				{7, 13},
+				{8, 21},
+				{9, 34},
+				{10, 55}
+		}
+		) {
+			Document fibonacci = new PseudoDocument(
+					"fibonacci.jh",
+					"    #if $p == 0",
+					"        #declare $r 0",
+					"    #elif $p == 1",
+					"        #declare $r 1",
+					"    #else",
+					"        #for $ort [$rt]",
+					"            #declare $p $p - 1",
+					"            #include __PATH__",
+					"            #declare $rt $r",
+					"",
+					"            #declare $p $p - 1",
+					"            #include __PATH__",
+					"            #declare $rt $rt + $r",
+					"",
+					"            #declare $p $p + 2",
+					"            #declare $r $rt",
+					"            #declare $rt $ort",
+					"        #endfor",
+					"    #endif"
+			);
+			Document main = new PseudoDocument(
+					"main.jamplate",
+					"    #declare $p $input",
+					"    #include __DIR__ 'fibonacci.jh'",
+					"    #message $r"
+			);
+
+			Unit unit = new UnitImpl(new GlucoseSpec());
+
+			unit.getSpec().add(listener(event -> {
+				if (event.getAction().equals(Action.PRE_EXEC))
+					event.getMemory().set(
+							"$input",
+							number(arr[0])
+					);
+			}));
+
+			OutputStream outBuffer = new ByteArrayOutputStream();
+			PrintStream out = System.out;
+			System.setOut(new PrintStream(outBuffer));
+
+			if (
+					!unit.initialize(fibonacci, main) ||
+					!unit.parse(fibonacci, main) ||
+					!unit.analyze(fibonacci, main) ||
+					!unit.compile(fibonacci, main) ||
+					!unit.execute(main)
+			) {
+				unit.diagnostic();
+				fail("Test not completed");
+			}
+
+			assertEquals(
+					Integer.toString(arr[1]),
+					outBuffer.toString(),
+					"Fibonacci seq"
+			);
+			System.setOut(out);
+		}
+	}
+
+	@Test
+	public void _quine() {
+		Document document = new PseudoDocument(
+				//name
+				"Main",
+				//content
+				"#declare input '#message \"#declare input \" \"\\'\" input \"\\'\" \"\\n\" input'",
+				"#message \"#declare input \" \"\\'\" input \"\\'\" \"\\n\" input"
+		);
+
+		Unit unit = new UnitImpl(new GlucoseSpec());
+
+		OutputStream outBuffer = new ByteArrayOutputStream();
+		PrintStream out = System.out;
+		System.setOut(new PrintStream(outBuffer));
+
+		if (
+				!unit.initialize(document) ||
+				!unit.parse(document) ||
+				!unit.analyze(document) ||
+				!unit.compile(document) ||
+				!unit.execute(document)
+		) {
+			unit.diagnostic();
+			fail("Test not completed");
+		}
+
+		assertEquals(
+				document.read(),
+				outBuffer.toString(),
+				"Weird Quine"
+		);
+		System.setOut(out);
 	}
 }
